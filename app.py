574046d5176fe5d7e6b2a2e512aa36749dc5b699
Pratham Manja <pratham0762002@gmail.com>
2023-06-15 11:03:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def calculate():
     return render_template('index.html', result=result)
 
 if __name__=="__main__":  
-    app.run(host='0.0.0.0', port=8080) 
+    app.run(host='0.0.0.0', port=8081) 
